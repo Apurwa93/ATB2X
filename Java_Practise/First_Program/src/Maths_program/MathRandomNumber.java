8eb7c80180d0ea5e4a5c4e5bd936bbcedd6ee00a
Apurwa93 <58219189+Apurwa93@users.noreply.github.com>
2023-05-03 17:37:39 +0530

    Add files via upload

---
+++ Java_Practise/First_Program/src/Maths_program/MathRandomNumber.java
@@ -0,0 +1,14 @@
+package Maths_program;
+
+public class MathRandomNumber {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 34;
+        int c = Math.max(a,b);
+        int d = Math.min(a,b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(Math.pow(a,b));
+
+    }
+}
---
+++ Java_Practise/First_Program/src/Programs/Boolean_test.java
@@ -0,0 +1,29 @@
+package Programs;
+
+public class Boolean_test {
+
+    public static void main(String[] args) {
+
+        boolean isMarried = false;
+
+        System.out.println(isMarried);
+
+        //Datatypes
+
+//        byte b = 10;
+//        short g = 11;
+//        int i = 121;
+//        long l = 100;
+//
+//        byte age = 127;
+//        int b,c;
+//        int a=b=c=99;
+//        System.out.println(c);
+
+//        boolean valid =true;
+//        System.out.println(valid);
+        int _123 = 99;
+        System.out.println(_123);
+    }
+
+}
