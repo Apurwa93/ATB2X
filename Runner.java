1cacde25e4734c10ec02dce2703709b712df59e2
Apurwa93 <58219189+Apurwa93@users.noreply.github.com>
2023-05-18 13:18:50 +0530

    Add files via upload

---
+++ Runner.java
@@ -0,0 +1,11 @@
+package Practice_19march.encapsulationsPractices;
+
+public class Runner {
+    public static void main(String[] args) {
+        Book b1 = new Book();
+        b1.setOfgffbfvfvfvofvfTitle("Head First java");
+        b1.setNoofcopies(-1);
+        System.out.println(b1.getTitle());
+
+    }
+}
